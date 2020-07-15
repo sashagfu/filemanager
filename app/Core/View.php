f64be17222918f3381e4c6b44b609ae3ed7a5de2
sashagfu <sashagfu@gmail.com>
2020-07-15 17:38:16 +0300

    Created Base View class

---
+++ app/Core/View.php
@@ -0,0 +1,44 @@
+<?php
+
+class View
+{
+	protected $data;
+
+	protected $path;
+
+	protected static function getDefaultViewPath()
+    {
+        $router = App::getRouter();
+        if (!$router) {
+            return false;
+        }
+        $controllerDir = $router->getController();
+        $templateName = $router->getAction() . '.php';
+
+        return VIEWS_PATH.DS.$controllerDir.DS.$templateName;
+    }
+
+	public function __construct($data = [], $path = null)
+    {
+        if (!$path) {
+            $path = self::getDefaultViewPath();
+        }
+        $this->path = $path;
+        $this->data = $data;
+    }
+
+    public function render()
+    {
+        $data = $this->data;
+
+        extract($data);
+
+        ob_start();
+        include($this->path);
+
+        $content = ob_get_contents();
+        @ob_end_clean();
+
+        return $content;
+    }
+}
