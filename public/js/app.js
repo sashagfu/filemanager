81c8e05bfbdcb040a4e2c94facbd75698cdae3c1
sashagfu <sashagfu@gmail.com>
2020-07-16 11:15:41 +0300

    Some changes

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,8 @@
-// get setting value
-// $(document).ready(function() {
-//   $.getJSON('/settings', function(response) {
-//     $('input[name="id"]').val(response.tz.id);
-//     $('input[name="name"]').val(response.tz.name);
-//     $('input[name="value"]').val(response.tz.value);
-//   })
-//     .fail(function() {
-//       console.log('error');
-//     });
-// });
-
 // update settings value
 $('#submit').click(function(e) {
   e.preventDefault();
-  // let id = $('input[name="id"]').val();
-  // let name = $('input[name="name"]').val();
   let value = $('input[name="value"]').val();
   $.post('/settings/edit', {
-    // id: id,
-    // name: name,
     value: value
   }, function(response) {
     $('.alert-success').html(response.message).show().delay(4000).fadeOut();
